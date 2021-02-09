e6082d153c2ac3c6cdc8aa122fb94a18e3b73897
Marco Parenzan <marco.parenzan@live.it>
2021-02-09 04:14:33 +0100

    Add project files.

---
+++ SlideComposerApp/SessionData.cs
@@ -0,0 +1,110 @@
+﻿namespace SlideComposerApp
+{
+    using System;
+    using System.Text.Json.Serialization;
+
+    public partial class SessionizeData
+    {
+        [JsonPropertyName("sessions")]
+        public Session[] Sessions { get; set; }
+
+        [JsonPropertyName("speakers")]
+        public Speaker[] Speakers { get; set; }
+
+        [JsonPropertyName("questions")]
+        public object[] Questions { get; set; }
+
+        [JsonPropertyName("categories")]
+        public object[] Categories { get; set; }
+
+        [JsonPropertyName("rooms")]
+        public Room[] Rooms { get; set; }
+    }
+
+    public partial class Room
+    {
+        [JsonPropertyName("id")]
+        public long Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("sort")]
+        public long Sort { get; set; }
+    }
+
+    public partial class Session
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("startsAt")]
+        public DateTimeOffset StartsAt { get; set; }
+
+        [JsonPropertyName("endsAt")]
+        public DateTimeOffset EndsAt { get; set; }
+
+        [JsonPropertyName("isServiceSession")]
+        public bool IsServiceSession { get; set; }
+
+        [JsonPropertyName("isPlenumSession")]
+        public bool IsPlenumSession { get; set; }
+
+        [JsonPropertyName("speakers")]
+        public Guid[] Speakers { get; set; }
+
+        [JsonPropertyName("categoryItems")]
+        public object[] CategoryItems { get; set; }
+
+        [JsonPropertyName("questionAnswers")]
+        public object[] QuestionAnswers { get; set; }
+
+        [JsonPropertyName("roomId")]
+        public long RoomId { get; set; }
+    }
+
+    public partial class Speaker
+    {
+        [JsonPropertyName("id")]
+        public Guid Id { get; set; }
+
+        [JsonPropertyName("firstName")]
+        public string FirstName { get; set; }
+
+        [JsonPropertyName("lastName")]
+        public string LastName { get; set; }
+
+        [JsonPropertyName("bio")]
+        public string Bio { get; set; }
+
+        [JsonPropertyName("tagLine")]
+        public string TagLine { get; set; }
+
+        [JsonPropertyName("profilePicture")]
+        public Uri ProfilePicture { get; set; }
+
+        [JsonPropertyName("isTopSpeaker")]
+        public bool IsTopSpeaker { get; set; }
+
+        [JsonPropertyName("links")]
+        public object[] Links { get; set; }
+
+        [JsonPropertyName("sessions")]
+        public long[] Sessions { get; set; }
+
+        [JsonPropertyName("fullName")]
+        public string FullName { get; set; }
+
+        [JsonPropertyName("categoryItems")]
+        public object[] CategoryItems { get; set; }
+
+        [JsonPropertyName("questionAnswers")]
+        public object[] QuestionAnswers { get; set; }
+    }
+}
---
+++ SlideComposerApp/Slide.cs
@@ -0,0 +1,88 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using SixLabors.Fonts;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SlideComposerApp
+{
+    public static class Slide
+    {
+        [FunctionName("Slide")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequestMessage req,
+            ILogger log)
+        {
+            var query = req.RequestUri.ParseQueryString();
+            var idx = int.Parse(query["idx"]);
+
+            var httpClient = new HttpClient();
+            var json = await httpClient.GetStringAsync("https://sessionize.com/api/v2/yhpzu19c/view/All");
+
+            var data = JsonSerializer.Deserialize<SessionizeData>(json);
+
+            FontCollection collection = new FontCollection();
+            // https://fonts.google.com/specimen/Roboto
+            var robotoLightFamily = collection.Install("assets/fonts/Roboto/Roboto-Light.ttf");
+            var titleFont = robotoLightFamily.CreateFont(64, FontStyle.Regular);
+            var speakerNameFont = robotoLightFamily.CreateFont(36, FontStyle.Regular);
+
+            var sessionData = data.Sessions[idx];
+            var speakerData = data.Speakers.FirstOrDefault(xx => xx.Id == sessionData.Speakers[0]);
+
+            var bannerPictureData = await File.ReadAllBytesAsync("assets/images/banner-tile.png");
+            var bannerPicture = Image.Load(bannerPictureData);
+
+            var speakerPictureData = await httpClient.GetByteArrayAsync(speakerData.ProfilePicture);
+            var speakerPicture = Image.Load(speakerPictureData);
+            speakerPicture.Mutate(xx => xx.Resize(360, 360));
+
+            var ms = new MemoryStream();
+
+            var color = Color.FromRgb(0x47, 0x93, 0xe0);
+
+            using (var image = new Image<Rgba32>(1920, 1080))
+            {
+                image.Mutate(x => x
+                    .DrawImage(bannerPicture, new Point(0, 0), 1)
+                    .DrawText(new TextGraphicsOptions(
+                        new GraphicsOptions
+                        {
+
+                        },
+                        new TextOptions
+                        {
+                            WrapTextWidth = 1200,
+                            VerticalAlignment = VerticalAlignment.Center
+                        }
+                    ), sessionData.Title.Trim('-', ' '), titleFont, color, new PointF(570, 588))
+                    .DrawImage(speakerPicture, new Point(85, 432), 1)
+                    .DrawText(new TextGraphicsOptions(
+                        new GraphicsOptions
+                        {
+
+                        },
+                        new TextOptions
+                        {
+                            HorizontalAlignment = HorizontalAlignment.Center
+                        }
+                    ), speakerData.FullName, speakerNameFont, color, new PointF(260, 800))
+                );
+                await image.SaveAsPngAsync(ms);
+            }
+
+            var result = new FileContentResult(ms.ToArray(), "image/png");
+            result.FileDownloadName = $"datasat0001-{idx}-{speakerData.FullName}.png";
+            return result;
+        }
+    }
+}
