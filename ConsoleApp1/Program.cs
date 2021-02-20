d16ba9218360422735ae3c161cb4a1ff99782935
Marco Parenzan <marco.parenzan@live.it>
2021-02-20 21:11:33 +0100

    Updated all

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,5 @@
+﻿using Sparkle.LinkedInNET;
+using System;
+
+var api = new LinkedInApi(new LinkedInApiConfiguration("77knuq0yahwxxy", "77knuq0yahwxxy"));
+api.
--- SlideComposerApp/Slide.cs
+++ SlideComposerApp/Slide.cs
@@ -20,62 +20,88 @@ namespace SlideComposerApp
         [FunctionName("Slide")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequestMessage req,
-            ILogger log)
+            ILogger log, ExecutionContext ctx)
         {
             var query = req.RequestUri.ParseQueryString();
-            var idx = int.Parse(query["idx"]);
+
+            var idx = 0;
+
+            var nameQuery = query["name"];
+
+            var idxQuery = query["idx"];
+            if (!string.IsNullOrWhiteSpace(idxQuery)) idx = int.Parse(idxQuery);
 
             var httpClient = new HttpClient();
             var json = await httpClient.GetStringAsync("https://sessionize.com/api/v2/yhpzu19c/view/All");
 
             var data = JsonSerializer.Deserialize<SessionizeData>(json);
 
+            Session sessionData = default;
+            Speaker speakerData = default;
+
+            if (string.IsNullOrWhiteSpace(nameQuery))
+            {
+                sessionData = data.Sessions[idx];
+                speakerData = data.Speakers.FirstOrDefault(xx => xx.Id == sessionData.Speakers[0]);
+            }
+            else
+            {
+                speakerData = data.Speakers.FirstOrDefault(xx => string.Compare(xx.FullName, nameQuery, true) == 0);
+                sessionData = data.Sessions.Where(xx => xx.Speakers.Any(yy => yy == speakerData.Id)).ToArray()[idx];
+            }
+
+            var bannerPictureData = await File.ReadAllBytesAsync(Path.Combine(ctx.FunctionAppDirectory, "assets/images/banner-tile.png"));
+            var bannerPicture = Image.Load(bannerPictureData);
+
+            byte[] speakerPictureData = default;
+            Image<Rgba32> speakerPicture = default;
+
+            if (speakerData.ProfilePicture != null)
+            {
+                await httpClient.GetByteArrayAsync(speakerData.ProfilePicture);
+                speakerPicture = Image.Load(speakerPictureData);
+                speakerPicture.Mutate(xx => xx.Resize(360, 360));
+            }
+
             FontCollection collection = new FontCollection();
             // https://fonts.google.com/specimen/Roboto
-            var robotoLightFamily = collection.Install("assets/fonts/Roboto/Roboto-Light.ttf");
+            var robotoLightFamily = collection.Install(Path.Combine(ctx.FunctionAppDirectory, "assets/fonts/Roboto/Roboto-Light.ttf"));
             var titleFont = robotoLightFamily.CreateFont(64, FontStyle.Regular);
             var speakerNameFont = robotoLightFamily.CreateFont(36, FontStyle.Regular);
 
-            var sessionData = data.Sessions[idx];
-            var speakerData = data.Speakers.FirstOrDefault(xx => xx.Id == sessionData.Speakers[0]);
-
-            var bannerPictureData = await File.ReadAllBytesAsync("assets/images/banner-tile.png");
-            var bannerPicture = Image.Load(bannerPictureData);
-
-            var speakerPictureData = await httpClient.GetByteArrayAsync(speakerData.ProfilePicture);
-            var speakerPicture = Image.Load(speakerPictureData);
-            speakerPicture.Mutate(xx => xx.Resize(360, 360));
+            var color = Color.FromRgb(0x47, 0x93, 0xe0);
 
             var ms = new MemoryStream();
 
-            var color = Color.FromRgb(0x47, 0x93, 0xe0);
-
             using (var image = new Image<Rgba32>(1920, 1080))
             {
-                image.Mutate(x => x
-                    .DrawImage(bannerPicture, new Point(0, 0), 1)
-                    .DrawText(new TextGraphicsOptions(
-                        new GraphicsOptions
-                        {
+                image.Mutate(x => {
+                        x
+                            .DrawImage(bannerPicture, new Point(0, 0), 1)
+                            .DrawText(new TextGraphicsOptions(
+                                new GraphicsOptions
+                                {
 
-                        },
-                        new TextOptions
-                        {
-                            WrapTextWidth = 1200,
-                            VerticalAlignment = VerticalAlignment.Center
-                        }
-                    ), sessionData.Title.Trim('-', ' '), titleFont, color, new PointF(570, 588))
-                    .DrawImage(speakerPicture, new Point(85, 432), 1)
-                    .DrawText(new TextGraphicsOptions(
-                        new GraphicsOptions
-                        {
+                                },
+                                new TextOptions
+                                {
+                                    WrapTextWidth = 1200,
+                                    VerticalAlignment = VerticalAlignment.Center
+                                }
+                            ), sessionData.Title.Trim('-', ' '), titleFont, color, new PointF(570, 588))
+                            .DrawText(new TextGraphicsOptions(
+                                new GraphicsOptions
+                                {
 
-                        },
-                        new TextOptions
-                        {
-                            HorizontalAlignment = HorizontalAlignment.Center
-                        }
-                    ), speakerData.FullName, speakerNameFont, color, new PointF(260, 800))
+                                },
+                                new TextOptions
+                                {
+                                    HorizontalAlignment = HorizontalAlignment.Center
+                                }
+                            ), speakerData.FullName, speakerNameFont, color, new PointF(260, 800));
+                        if (speakerPicture != null)
+                            x.DrawImage(speakerPicture, new Point(85, 432), 1);
+                    }
                 );
                 await image.SaveAsPngAsync(ms);
             }
